4ccdf63701ef82fef409fd30f6d54b167d580b42
xiaoqingsong <xiaoqingsong@jd.com>
2017-05-03 19:17:17 +0800

    delete some files

--- app/src/main/java/com/xqs/drawerdemo/HomeActivity.java
+++ app/src/main/java/com/xqs/drawerdemo/HomeActivity.java
@@ -33,6 +33,7 @@ public class HomeActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setItemIconTintList(null);
     }
 
     @Override
